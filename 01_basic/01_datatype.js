1798caecea42286e94d67f2ba6a76190b4ae36db
Ahasanul Hoque <109936616+hoquecse@users.noreply.github.com>
2024-08-02 15:09:52 +0000

    daatatype

---
+++ 01_basic/01_datatype.js
@@ -0,0 +1,13 @@
+"use strict"
+
+console.log(3+3)
+console.log("Ahasanul");
+// window.alert("Ahasanul");
+// alert("Ahasanul");
+
+let name="Ahsanul"
+let age=25
+let logdin=true
+let strin;
+
+console.log(typeof null);
