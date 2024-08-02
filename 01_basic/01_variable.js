1bcee57679eb1e45c19a8d7a6ec3d1e9d9d614db
Ahasanul Hoque <109936616+hoquecse@users.noreply.github.com>
2024-08-02 16:11:29 +0000

    converion of datatype

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,16 @@
+const accountId = 1245887;
+let accountEmail ="hhoquesce@gmail.com"
+var accountPassword = "12545787"
+
+accountCity = "Farakka"
+let accountState;
+
+// accountId=21; 
+accountEmail="hhdgg@gmail.com"
+accountPassword="2121"
+accountCity= "hi"
+
+console.log(accountCity);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
