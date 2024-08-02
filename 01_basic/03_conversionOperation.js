e08c484892f9c99714689835261c3fb2e5eaa1a9
Ahasanul Hoque <109936616+hoquecse@users.noreply.github.com>
2024-08-02 16:14:46 +0000

    conversion of datatype

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,24 @@
+// let score = "false";
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueinNumber=Number(score);
+// console.log(typeof valueinNumber);
+// console.log(valueinNumber);
+
+// let boolean= "Ahasanul";
+
+// console.log(typeof boolean);
+// let valueinBoolean= Boolean(boolean);
+// console.log(typeof valueinBoolean);
+
+let stringNumber=23;
+
+let valueinString=String(stringNumber);
+
+console.log(typeof valueinString);
+
+
+
+
