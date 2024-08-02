aa6bbb4afb042824af789ff3510837ae8ea5a282
Ahasanul Hoque <109936616+hoquecse@users.noreply.github.com>
2024-08-02 17:30:16 +0000

    conversion of datatype

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -17,7 +17,49 @@ let stringNumber=23;
 
 let valueinString=String(stringNumber);
 
-console.log(typeof valueinString);
+// console.log(typeof valueinString);
+
+// ..........................Operation..............//
+
+let value=2;
+
+let negValue=-value;
+// console.log(negValue);
+
+let name1="hello"
+let name2=" Ahasanul"
+let name3=name1+name2;
+// console.log(name3);
+
+// console.log(1+2);
+// console.log(1+"2");
+// console.log("1"+3);
+// console.log("1"+3+9);
+// console.log(1+2+"5");
+
+
+console.log((2+3)*5/2);
+
+// console.log(+true);
+
+let num1,num2,num3;
+num1=num2=num3 = 2 + 2;
+// console.log(num3);
+console.table([num1,num2,num3]);
+
+let val=2;
+val++;
+
+console.log(val);
+
+
+
+
+
+
+
+
+
 
 
 
