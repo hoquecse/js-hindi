47b2368a77c7c617ba4cfdd3951fda5e52cdc6a4
Ahasanul Hoque <109936616+hoquecse@users.noreply.github.com>
2024-08-05 16:31:58 +0000

    datatype_summary

---
+++ 01_basic/datatype_summary.js
@@ -0,0 +1,29 @@
+const id = Symbol('123');
+const anotherId = Symbol('123');
+
+// console.log(id);
+// console.log(anotherId);
+// console.log(id == anotherId);
+
+
+const bigNumber = 124578998646;
+
+// console.log(bigNumber);
+
+const hero = ["hdgg", "h,kwuia", "kjsh"];
+
+let object1={
+    name:"Ahasanul",
+    age:25,
+};
+let n= null;
+let myob=function(){
+    console.log("hello");
+    
+}
+
+console.log(typeof n);
+
+
+
+
